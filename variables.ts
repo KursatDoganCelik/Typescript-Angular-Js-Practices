743c606b75bd09e79400c3ac258371dc8651c72c
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-06 16:25:05 +0300

    all necessary variables added

---
+++ variables.ts
@@ -0,0 +1,45 @@
+// Number
+let number: number = 10
+
+// String
+let string: string = 'Hello'
+
+// Boolean
+let boolean: boolean = true
+
+// Array
+let array: number[] = [1, 2, 3]
+
+// Tuple
+let tuple: [number, string] = [4, 'Heyyo']
+
+// Object
+let object: object = { name: 'Soligin' }
+
+// Null
+let nullType: null = null
+
+// Undefined
+let undefinedType: undefined = undefined
+
+// Union
+let unionType: number | string = 'Heyyo'
+
+// Literal
+let literalType: 7 = 7
+
+// Any (Avoid using it)
+let any: any = 'Heyyo World!'
+
+// Console.log Parts
+console.log(`Number: ${number}`)
+console.log(`String: ${string}`)
+console.log(`Boolean: ${boolean}`)
+console.log(`Array: ${array}`)
+console.log(`Tuple: ${tuple}`)
+console.log(`Object: ${object}`)
+console.log(`Null: ${nullType}`)
+console.log(`Undefined: ${undefinedType}`)
+console.log(`Union: ${unionType}`)
+console.log(`Literal: ${literalType}`)
+console.log(`Any: ${any}`)
