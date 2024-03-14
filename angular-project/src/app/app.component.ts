04018ccc03e82a012269a193766cc798d55614ff
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-14 18:40:11 +0300

    name component imported

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NameComponent } from './name/name.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NameComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-project';
